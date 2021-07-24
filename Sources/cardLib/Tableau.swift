41dfdd3686046a68c42269c7c1497cd548264669
John Varney <john@jdvarney.com>
2021-07-24 09:33:18 -0400

    Review

--- Sources/cardLib/Tableau.swift
+++ Sources/cardLib/Tableau.swift
@@ -9,22 +9,24 @@ import Foundation
 
 public struct Tableau {
     public var columns = [Column]()
-    
+    public var draw = Draw()
+    private var deck = Deck()
+
     public init () {                       /// Build 7 card columns for the solitaire tableau
-        var deck = Deck()
-        var subDeck: [Card] = Array(deck.deal(count: 28)
+        let subDeck: [Card] = Array(deck.deal(count: 28))
         
         for cardCount in 1...7 {
-            var newCards = subDeck!.prefix(cardCount)
-            newCards.removeFirst(cardCount)
+            var newCards = subDeck.prefix(cardCount)
+            newCards.removeFirst(cardCount)            
             
             var tempCard = newCards.last
             tempCard?.encumbered = false
             tempCard?.faceShowing = true
         
-            let newColumn = Column.init(cards: newCards, colIndex: cardCount)
+            let newColumn = Column.init(cards: Array(newCards), colIndex: cardCount)
         
             columns.append(newColumn)
         }
+        draw.cards = Array(deck.deal(count:24)) //Remaining cards go into draw pile
     }
 }
