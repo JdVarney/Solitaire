0ce2905c2fbccc70597fec9a3dc2f55e606f5340
John Varney <john@jdvarney.com>
2021-08-15 09:20:35 -0400

    Fixed issue with columns not being loaded

--- Sources/cardLib/Tableau.swift
+++ Sources/cardLib/Tableau.swift
@@ -13,11 +13,11 @@ public struct Tableau {
     private var deck = Deck()
 
     public init () {                       /// Build 7 card columns for the solitaire tableau
-        let subDeck: [Card] = Array(deck.deal(count: 28))
+        var subDeck: [Card] = Array(deck.deal(count: 28))
         
         for cardCount in 1...7 {
-            var newCards = subDeck.prefix(cardCount)
-            newCards.removeFirst(cardCount)            
+            let newCards = subDeck.prefix(cardCount)
+            subDeck.removeFirst(cardCount)
             
             var tempCard = newCards.last
             tempCard?.encumbered = false
