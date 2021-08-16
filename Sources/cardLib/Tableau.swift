976f8044e4199fab52ccb2bb0104ae59a6ee4ceb
John Varney <john@jdvarney.com>
2021-08-15 23:23:56 -0400

    update to show face cards

--- Sources/cardLib/Tableau.swift
+++ Sources/cardLib/Tableau.swift
@@ -12,21 +12,19 @@ public struct Tableau {
     public var draw = Draw()
     private var deck = Deck()
 
-    public init () {                       /// Build 7 card columns for the solitaire tableau
+    public init () {             // Build 7 card columns for the solitaire tableau
+        
         var subDeck: [Card] = Array(deck.deal(count: 28))
         
         for cardCount in 1...7 {
-            let newCards = subDeck.prefix(cardCount)
+            var newCards = subDeck.prefix(cardCount)
+            newCards[cardCount - 1].faceShowing = true
             subDeck.removeFirst(cardCount)
             
-            var tempCard = newCards.last
-            tempCard?.encumbered = false
-            tempCard?.faceShowing = true
-        
             let newColumn = Column.init(cards: Array(newCards), colIndex: cardCount)
-        
             columns.append(newColumn)
         }
+        
         draw.cards = Array(deck.deal(count:24)) //Remaining cards go into draw pile
     }
 }
